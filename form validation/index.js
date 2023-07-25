01181e0f0d80e542df5a329ec8c90c6fa64c83d8
Madhuresh123 <madhuresh91394@gmail.com>
2023-07-25 10:55:14 +0530

    form validation completed

--- form validation/index.js
+++ form validation/index.js
@@ -1,43 +1,58 @@
+function setError(id, error) {
+  //handle input errors
+  const element = document.getElementById(id);
+  const showError = element.getElementsByClassName("form_error")[0];
 
-function setError(id, error){
-    //handle input errors
-    const element = document.getElementById(id);
-    const showError = element.getElementsByClassName("form_error")[0];
-    showError.innerHTML = error;
+  let showOutline = element.getElementsByClassName("input_field")[0];
 
+  showError.innerHTML = error;
+
+  if(error.length == 0){
+    showOutline.style.border = "2px solid green";
+    showOutline.style.outline = "none";
+
+  }else{
+    showOutline.style.border = "2px solid red";
+    showOutline.style.outline = "none";
+  }
 }
 
-function validate_Form(){
-    // handle form validation
+function validate_Form() {
+  // handle form validation
 
+  let submiteFrom = true;
 
-   let submiteFrom = true;
+  const username = document.forms["myForm"]["username"].value;
+  const password = document.forms["myForm"]["password"].value;
+  const confirm_password = document.forms["myForm"]["confirm_password"].value;
 
-    const username = document.forms["myForm"]["username"].value;
-    const password = document.forms["myForm"]["password"].value;
-    const confirm_password = document.forms["myForm"]["confirm_password"].value;
+  // if usename length is smaller than 5 character form will not submite.
+  if (username.length < 5) {
+    setError(
+      "username",
+      "*username is too short (must be greater than 5 character)"
+    );
+    submiteFrom = false;
+  } else {
+    setError("username", "");
+  }
 
+  if (password.length < 8) {
+    setError(
+      "password",
+      "*password is too short (must be greater than 8 character)"
+    );
+    submiteFrom = false;
+  } else {
+    setError("password", "");
+  }
 
+  if (password != confirm_password) {
+    setError("confirm_password", "*password is not same");
+    submiteFrom = false;
+  } else {
+    setError("confirm_password", "");
+  }
 
-    // if usename length is smaller than 5 character form will not submite.
-    if(username.length < 5){
-        setError("username", "*username is too short (must be greater than 5 character)");
-        submiteFrom = false;
-    }
-
-    if(password.length < 8){
-        setError("password", "*password is too short (must be greater than 8 character)");
-        submiteFrom = false;
-    }
-
-    if(password != confirm_password ){
-        setError("confirm_password", "*password is not same");
-        submiteFrom = false;
-    }
-
-    return submiteFrom; 
-
-
-
-
+  return submiteFrom;
 }
